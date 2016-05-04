a8405e9a244bf776684f1d0eecfd9ce5dab42cb5
Aki <nthirtyone@gmail.com>
2016-05-04 16:06:42 +0200

    Classname labeling.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ xml["normal"] = "\t\t\t\t\t\t<condition id=\"isUpgradeEnabled\">\n\t\t\t\t\t\t\t
 xml["ordered-start"] = "\t\t\t\t\t\t<condition id=\"isUpgradeEnabled\">\n\t\t\t\t\t\t\t<string id=\"condition\" values=\"yesno\">no</string>\n\t\t\t\t\t\t\t<string id=\"upgrade name\">$upgrade</string>\n\t\t\t\t\t\t\t<normal>\n\t\t\t\t\t\t\t\t<condition id=\"canPayUpgrade\">\n\t\t\t\t\t\t\t\t\t<string id=\"upgrade name\">$upgrade</string>\n\t\t\t\t\t\t\t\t\t<normal>\n\t\t\t\t\t\t\t\t\t\t<action id=\"buyUpgrade\">\n\t\t\t\t\t\t\t\t\t\t\t<string id=\"upgrade name\">$upgrade</string>\n\t\t\t\t\t\t\t\t\t\t</action>\n\t\t\t\t\t\t\t\t\t</normal>\n\t\t\t\t\t\t\t\t</condition>\n\t\t\t\t\t\t\t</normal>\n\t\t\t\t\t\t\t<else>\n";
 xml["ordered-end"] = "\t\t\t\t\t\t\t</else>\n\t\t\t\t\t\t</condition>\n";
 
+/* 
+ * We are generating XMLs here.
+ * And we are doing it the most barbaric way, because they are just text after all.
+ */
+
 var app = angular.module('AI-Build-Generator', []);
 app.controller('AI-Build-Controller', function($scope, $http) {
 	$scope.build = "";
@@ -40,12 +45,23 @@ app.controller('AI-Build-Controller', function($scope, $http) {
         var file = "json/" + name + ".json";
         try {
             $http.get(file).then(function(response) {
-                var foo = xml["start"];
+                var foo = "";
                 for (var i = 0; i < $scope.items().length; i++) {
-                    foo += xml["normal"].replace(/\$upgrade/g, response.data.items[$scope.items()[i]]);
+					// $scope.items()[i] - current item number
+					// response.data.items - buyable items array
+					var a = response.data.items[$scope.items()[i]].classname;
+					if (!response.data.items[$scope.items()[i]].notlabeled) {
+						if (!response.data.items[$scope.items()[i]].label) {
+							response.data.items[$scope.items()[i]].label = 1;
+						}
+						else {
+							response.data.items[$scope.items()[i]].label++;
+						}
+						a += response.data.items[$scope.items()[i]].label;
+					}
+                    foo += xml["normal"].replace(/\$upgrade/g, a);
                 }
-				foo += xml["end"];
-                $scope.output = foo;
+                $scope.output = xml["start"] + foo + xml["end"];
             }, function(response){throw "shit";}
             );
         }

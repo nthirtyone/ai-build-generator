8d9aedd0c3eee6b87168f102c6bc979e8f79d954
Aki <nthirtyone@gmail.com>
2017-04-29 16:39:11 +0200

    Added Deadlift

--- js/main.js
+++ js/main.js
@@ -41,7 +41,8 @@ app.controller('AI-Build-Controller', function($scope, $http) {
 						{name: "Voltar the Omniscient", filename: "Voltar_the_Omniscient"},
 						{name: "Vinnie & Spike", filename: "Vinnie_&_Spike"},
 						{name: "Ix the Interloper", filename: "IX_the_Interloper"},
-						{name: "Max Focus", filename: "Max_Focus"}
+						{name: "Max Focus", filename: "Max_Focus"},
+						{name: "Deadlift", filename: "Deadlift"}
 					   ];
 	$scope.build = "";
     $scope.data  = function() {
